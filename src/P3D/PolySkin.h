550f333498fff9c0e2122b183db2cf69cad9a5a0
Matt Stevens <matt@handsomematt.co.uk>
2019-07-09 03:06:31 +0100

    PolySkin hold data

--- src/P3D/PolySkin.h
+++ src/P3D/PolySkin.h
@@ -3,6 +3,15 @@
 namespace Donut::P3D {
 
 class PolySkin {
+public:
+	PolySkin(const std::string& name, const std::string& skeletonName) :
+		_name(name), _skeletonName(skeletonName) {}
+
+	std::string& GetName() { return _name; }
+	std::string& GetSkeletonName() { return _skeletonName; }
+private:
+	std::string _name;
+	std::string _skeletonName;
 };
 
 } // namespace Donut::P3D
