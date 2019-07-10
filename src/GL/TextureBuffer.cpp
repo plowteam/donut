728375ac673d45c3071056eaf43feb5c2e2a763f
aylaylay <fortblox@gmail.com>
2019-07-10 22:53:14 +0100

    Don't keep a copy of texture buffer data

--- src/GL/TextureBuffer.cpp
+++ src/GL/TextureBuffer.cpp
@@ -3,69 +3,45 @@
 
 namespace Donut::GL {
 
-TextureBuffer::Data::Data() : m_target(GL_TEXTURE_BUFFER) {}
+TextureBuffer::TextureBuffer() :
+	m_handle(0),
+	m_bufferHandle(0) {
+	glGenTextures(1, &m_handle);
+	glGenBuffers(1, &m_bufferHandle);
 
-TextureBuffer::TextureBuffer() : m_handle(0), m_bufferHandle(0) {
-    Load();
+	glBindBuffer(GL_TEXTURE_BUFFER, m_bufferHandle);
+	glBufferData(GL_TEXTURE_BUFFER, 0, 0, GL_DYNAMIC_READ);
+	glBindBuffer(GL_TEXTURE_BUFFER, 0);
+
+	glBindTexture(GL_TEXTURE_BUFFER, m_handle);
+	glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, m_bufferHandle);
+	glBindTexture(GL_TEXTURE_BUFFER, 0);
 }
 
 TextureBuffer::~TextureBuffer() {
-    Unload();
-}
+	if (m_bufferHandle != 0) {
+		glDeleteBuffers(1, &m_bufferHandle);
+		m_bufferHandle = 0;
+	}
 
-void TextureBuffer::Load() {
-    glGenTextures(1, &m_handle);
-    glGenBuffers(1, &m_bufferHandle);
-
-	glBindBuffer(m_data.m_target, m_bufferHandle);
-	glBufferData(m_data.m_target, 0, 0, GL_DYNAMIC_READ);
-	glBindBuffer(m_data.m_target, 0);
-	
-	glBindTexture(m_data.m_target, m_handle);
-    glTexBuffer(m_data.m_target, GL_RGBA32F, m_bufferHandle);
-    glBindTexture(m_data.m_target, 0);
-}
-
-void TextureBuffer::Unload() {
-
-    if (m_bufferHandle != 0) {
-        glDeleteBuffers(1, &m_bufferHandle);
-        m_bufferHandle = 0;
-    }
-
-    if (m_handle != 0) {
-        glDeleteTextures(1, &m_handle);
-        m_handle = 0;
-    }
-}
-
-void TextureBuffer::Update() {
-    glBindBuffer(m_data.m_target, m_bufferHandle);
-    glBufferData(m_data.m_target, m_data.m_buffer.size(), &m_data.m_buffer[0], GL_DYNAMIC_READ);
-    glBindBuffer(m_data.m_target, 0);
+	if (m_handle != 0) {
+		glDeleteTextures(1, &m_handle);
+		m_handle = 0;
+	}
 }
 
 void TextureBuffer::SetBuffer(void* buffer, unsigned int length) {
-    if (m_data.m_buffer.size() != length) {
-        m_data.m_buffer.clear();
-        m_data.m_buffer.resize(length);
-    }
-
-    memcpy(&m_data.m_buffer[0], buffer, length);
-
-    Update();
-}
-
-void TextureBuffer::SetBuffer(Data::Buffer& buffer) {
-    SetBuffer((unsigned char*)&buffer[0], buffer.size());
+	glBindBuffer(GL_TEXTURE_BUFFER, m_bufferHandle);
+	glBufferData(GL_TEXTURE_BUFFER, length, buffer, GL_DYNAMIC_READ);
+	glBindBuffer(GL_TEXTURE_BUFFER, 0);
 }
 
 void TextureBuffer::Bind() {
-    glBindTexture(m_data.m_target, m_handle);
+    glBindTexture(GL_TEXTURE_BUFFER, m_handle);
 }
 
 void TextureBuffer::Unbind() {
-    glBindTexture(m_data.m_target, 0);
+    glBindTexture(GL_TEXTURE_BUFFER, 0);
 }
 
 } // namespace Donut::GL
--- src/GL/TextureBuffer.h
+++ src/GL/TextureBuffer.h
@@ -8,34 +8,14 @@ namespace Donut::GL {
 class TextureBuffer {
 
 public:
-    struct Data {
-        Data();
-
-        typedef std::vector<unsigned char> Buffer;
-
-		GLuint m_target;
-        Buffer m_buffer;
-    };
-
     TextureBuffer();
 	~TextureBuffer();
 
     void Bind();
     void Unbind();
-
     void SetBuffer(void* buffer, unsigned int length);
-    void SetBuffer(Data::Buffer& buffer);
-
-    inline Data::Buffer& GetBuffer() {
-        return m_data.m_buffer;
-    }
 
 protected:
-    virtual void Load();
-    virtual void Unload();
-    virtual void Update();
-
-    Data m_data;
     GLuint m_handle;
     GLuint m_bufferHandle;
 };
