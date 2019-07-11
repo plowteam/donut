8d7bbd8b450fb37d0795879306691254bdfeab46
aylaylay <fortblox@gmail.com>
2019-07-11 04:58:50 +0100

    read quaternion and compressed quaternion

--- src/P3D/CompressedQuaternionChannel.cpp
+++ src/P3D/CompressedQuaternionChannel.cpp
@@ -8,5 +8,20 @@ namespace Donut::P3D {
 	{
 		AnimChannel::Read(stream);
 
+		_numberOfFrames = stream.Read<std::uint32_t>();
+
+		_frames.resize(_numberOfFrames);
+		stream.ReadBytes(reinterpret_cast<uint8_t*>(_frames.data()), _numberOfFrames * sizeof(uint16_t));
+
+		_values.resize(_numberOfFrames);
+		for (uint32_t i = 0; i < _numberOfFrames; ++i)
+		{
+			float w = (float)stream.Read<int16_t>() / (float)SHRT_MAX;
+			float x = (float)stream.Read<int16_t>() / (float)SHRT_MAX;
+			float y = (float)stream.Read<int16_t>() / (float)SHRT_MAX;
+			float z = (float)stream.Read<int16_t>() / (float)SHRT_MAX;
+
+			_values[i] = glm::vec4(x, y, z, w);
+		}
 	}
 } // namespace Donut::P3D
--- src/P3D/QuaternionChannel.cpp
+++ src/P3D/QuaternionChannel.cpp
@@ -8,5 +8,20 @@ namespace Donut::P3D {
 	{
 		AnimChannel::Read(stream);
 
+		_numberOfFrames = stream.Read<std::uint32_t>();
+
+		_frames.resize(_numberOfFrames);
+		stream.ReadBytes(reinterpret_cast<uint8_t*>(_frames.data()), _numberOfFrames * sizeof(uint16_t));
+
+		_values.resize(_numberOfFrames);
+		for (uint32_t i = 0; i < _numberOfFrames; ++i)
+		{
+			float w = stream.Read<float>();
+			float x = stream.Read<float>();
+			float y = stream.Read<float>();
+			float z = stream.Read<float>();
+
+			_values[i] = glm::vec4(x, y, z, w);
+		}
 	}
 } // namespace Donut::P3D
