da1c9cbc31e3e5d650f4059b8629bfbdfa0c644e
aylaylay <fortblox@gmail.com>
2019-07-11 04:49:55 +0100

    create the other channels

--- src/P3D/AnimationGroup.cpp
+++ src/P3D/AnimationGroup.cpp
@@ -15,7 +15,10 @@ namespace Donut::P3D {
 		uint32_t groupId = stream.Read<uint32_t>();
 		uint32_t numChannels = stream.Read<uint32_t>();
 
+		std::unique_ptr<Vector2Channel> vector2Channel = nullptr;
 		std::unique_ptr<Vector3Channel> vector3Channel = nullptr;
+		std::unique_ptr<QuaternionChannel> quaternionChannel = nullptr;
+		std::unique_ptr<CompressedQuaternionChannel> compressedQuaternionChannel = nullptr;
 
 		for (auto const& child : chunk.GetChildren())
 		{
@@ -26,14 +29,20 @@ namespace Donut::P3D {
 			case ChunkType::Vector1Channel:
 				break;
 			case ChunkType::Vector2Channel:
+				vector2Channel = std::make_unique<Vector2Channel>();
+				vector2Channel->Read(data);
 				break;
 			case ChunkType::Vector3Channel:
 				vector3Channel = std::make_unique<Vector3Channel>();
 				vector3Channel->Read(data);
 				break;
 			case ChunkType::QuaternionChannel:
+				quaternionChannel = std::make_unique<QuaternionChannel>();
+				quaternionChannel->Read(data);
 				break;
 			case ChunkType::CompressedQuaternionChannel:
+				compressedQuaternionChannel = std::make_unique<CompressedQuaternionChannel>();
+				compressedQuaternionChannel->Read(data);
 				break;
 			default:
 				std::cout << "Unexpected Chunk: " << child->GetType() << "\n";
