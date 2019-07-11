2e9ecc6d1846a27f88cb7369c29033b89f6c4529
aylaylay <fortblox@gmail.com>
2019-07-11 04:14:16 +0100

    Add channel unique ptrs

--- src/P3D/AnimationGroup.h
+++ src/P3D/AnimationGroup.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <P3D/P3DChunk.h>
+#include <P3D/Vector2Channel.h>
+#include <P3D/Vector3Channel.h>
+#include <P3D/QuaternionChannel.h>
+#include <P3D/CompressedQuaternionChannel.h>
 #include <memory>
 
 namespace Donut::P3D {
@@ -11,6 +15,11 @@ public:
 
 	static std::unique_ptr<AnimationGroup> Load(const P3DChunk&);
 private:
+
+	std::unique_ptr<Vector2Channel> _vector2Channel;
+	std::unique_ptr<Vector3Channel> _vector3Channel;
+	std::unique_ptr<QuaternionChannel> _quaternionChannel;
+	std::unique_ptr<CompressedQuaternionChannel> _compressedQuaternionChannel;
 };
 
 } // namespace Donut::P3D
