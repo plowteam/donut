b51c0696ddd5544c8fa16e9ce26366c54677d65a
Matt Stevens <matt@handsomematt.co.uk>
2019-07-13 23:43:57 +0100

    read some more of collision object

--- src/P3D/CollisionObject.cpp
+++ src/P3D/CollisionObject.cpp
@@ -1,8 +1,7 @@
 #include <MemoryStream.h>
 #include <P3D/CollisionObject.h>
-
-#include <iostream>
 #include <glm/gtc/type_ptr.inl>
+#include <iostream>
 
 namespace Donut::P3D
 {
@@ -18,7 +17,13 @@ std::unique_ptr<CollisionObject> CollisionObject::Load(const P3DChunk& chunk)
 	uint32_t objectCount = stream.Read<uint32_t>();
 	uint32_t ownerCount  = stream.Read<uint32_t>();
 
-	std::cout << name << " obj count: " << objectCount << " owner count: " << ownerCount << "\n";
+	for (auto const& volumeChunk : chunk.GetChildren())
+	{
+		if (!volumeChunk->IsType(ChunkType::CollisionVolume))
+			continue;
+
+		auto volume = CollisionVolume::Load(*volumeChunk);
+	}
 
 	return std::make_unique<CollisionObject>(name, nullptr);
 }
@@ -26,23 +31,27 @@ std::unique_ptr<CollisionObject> CollisionObject::Load(const P3DChunk& chunk)
 CollisionBoundingBox CollisionBoundingBox::Load(const P3DChunk& chunk)
 {
 	assert(chunk.IsType(ChunkType::CollisionBoundingBox));
-	
+
 	auto const size = glm::make_vec3(reinterpret_cast<const float*>(chunk.GetData().data()));
 
 	// should be: Centre, RotX, RotY, RotZ
 	auto& children = chunk.GetChildren();
 	assert(children.size() == 4);
-	
+
 	auto const centre = glm::make_vec3(reinterpret_cast<const float*>(children.at(0)->GetData().data()));
 	auto const rotX   = glm::make_vec3(reinterpret_cast<const float*>(children.at(0)->GetData().data()));
 	auto const rotY   = glm::make_vec3(reinterpret_cast<const float*>(children.at(0)->GetData().data()));
 	auto const rotZ   = glm::make_vec3(reinterpret_cast<const float*>(children.at(0)->GetData().data()));
-	
+
 	return CollisionBoundingBox(size, centre, rotX, rotY, rotZ);
 }
 
-std::unique_ptr<CollisionVolume> CollisionVolume::Load(const P3DChunk&)
+std::unique_ptr<CollisionVolume> CollisionVolume::Load(const P3DChunk& chunk)
 {
+	assert(chunk.IsType(ChunkType::CollisionVolume));
+
+	// std::cout << chunk.GetChildren().size() << "\n";
+
 	return std::unique_ptr<CollisionVolume>();
 }
 
