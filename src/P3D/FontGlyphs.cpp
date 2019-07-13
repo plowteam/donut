dda16e9dcfc3ef3df98649e1d7a84f87b11b4b8a
aylaylay <fortblox@gmail.com>
2019-07-14 00:07:03 +0100

    read font glyphs

--- src/P3D/FontGlyphs.cpp
+++ src/P3D/FontGlyphs.cpp
@@ -11,6 +11,9 @@ namespace Donut::P3D
 		MemoryStream stream(chunk.GetData());
 
 		uint32_t numGlyphs = stream.Read<uint32_t>();
+		std::vector<FontGlyph> glyphs;
+		glyphs.resize(numGlyphs);
+		stream.ReadBytes(reinterpret_cast<uint8_t*>(glyphs.data()), numGlyphs * sizeof(FontGlyph));
 
 		return std::make_unique<FontGlyphs>();
 	}
--- src/P3D/FontGlyphs.h
+++ src/P3D/FontGlyphs.h
@@ -4,6 +4,20 @@
 
 namespace Donut::P3D
 {
+	struct FontGlyph
+	{
+		uint32_t textureId;
+		float bottomLeftX;
+		float bottomLeftY;
+		float topRightX;
+		float topRightY;
+		float leftBearing;
+		float rightBearing;
+		float width;
+		float advance;
+		uint32_t id;
+	};
+
 	class FontGlyphs
 	{
 	public:
