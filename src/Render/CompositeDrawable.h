7e86b0d274d9b5e0536211ef482d23f5c25e1727
aylaylay <fortblox@gmail.com>
2019-07-21 08:53:16 +0100

    whoopsie didnt save

--- src/Render/CompositeDrawable.h
+++ src/Render/CompositeDrawable.h
@@ -1,27 +1,19 @@
 #pragma once
 
 #include <Render/Mesh.h>
-#include <P3D/StaticEntity.h>
 #include <ResourceManager.h>
 #include <string>
 
 namespace Donut
 {
+	class CompositeDrawable
+	{
+	public:
 
-// StaticEntity is just a Mesh wrapper, maybe replace it?
-class StaticEntity
-{
-  public:
-	  StaticEntity(const P3D::StaticEntity&);
-	  StaticEntity(const std::string& name, const P3D::Mesh&, const glm::mat4&);
+		void Draw(const ResourceManager&, glm::mat4& viewProj);
+		void LoadP3D(const std::string& filename);
 
-	void Draw(const GL::ShaderProgram&, const ResourceManager&) const;
+	private:
 
-	const glm::mat4& GetTransform() const { return _transform; }
-  private:
-	std::string _name;
-	std::unique_ptr<Mesh> _mesh;
-	glm::mat4 _transform;
-};
-
+	};
 } // namespace Donut
