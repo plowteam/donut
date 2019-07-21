af2addb27df0e5f027cecaed0b36c269ec145e3d
aylaylay <fortblox@gmail.com>
2019-07-21 11:57:04 +0100

    use GL_LINEAR_MIPMAP_LINEAR

--- src/Render/OpenGL/Texture2D.cpp
+++ src/Render/OpenGL/Texture2D.cpp
@@ -10,12 +10,13 @@ Texture2D::Texture2D(GLsizei width, GLsizei height, GLenum internalFormat, GLenu
 
 	glBindTexture(GL_TEXTURE_2D, _textureID);
 	glTexImage2D(GL_TEXTURE_2D, 0, _internalFormat, _width, _height, 0, _format, _type, textureData);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	// todo: expose this
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
 }
 
 Texture2D::~Texture2D()
