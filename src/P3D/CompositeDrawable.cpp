7bfc4777b458e294ab1123828883cd45e02f7865
aylaylay <fortblox@gmail.com>
2019-07-21 08:18:32 +0100

    CompositeDrawable loader shite

--- src/P3D/CompositeDrawable.cpp
+++ src/P3D/CompositeDrawable.cpp
@@ -10,13 +10,17 @@ namespace Donut::P3D
 
 		MemoryStream stream(chunk.GetData());
 
-		std::string name = stream.ReadLPString();
+		auto name = stream.ReadLPString();
+		auto skeletonName = stream.ReadLPString();
+
+		std::unique_ptr<CompositeDrawablePropList> propList;
 
 		for (auto const& child : chunk.GetChildren())
 		{
 			switch (child->GetType())
 			{
 			case ChunkType::CompositeDrawablePropList:
+				propList = CompositeDrawablePropList::Load(*child);
 				break;
 			case ChunkType::CompositeDrawableSkinList:
 				break;
@@ -27,6 +31,53 @@ namespace Donut::P3D
 			}
 		}
 
-		return std::make_unique<CompositeDrawable>();
+		return std::make_unique<CompositeDrawable>(name, skeletonName, std::move(propList));
+	}
+
+	std::unique_ptr<CompositeDrawablePropList> CompositeDrawablePropList::Load(const P3DChunk& chunk)
+	{
+		assert(chunk.IsType(ChunkType::CompositeDrawablePropList));
+
+		MemoryStream stream(chunk.GetData());
+
+		auto numElements = stream.Read<uint32_t>();
+		std::vector<std::unique_ptr<CompositeDrawableProp>> props;
+
+		for (auto const& child : chunk.GetChildren())
+		{
+			switch (child->GetType())
+			{
+			case ChunkType::CompositeDrawableProp:
+				props.push_back(CompositeDrawableProp::Load(*child));
+				break;
+			default:
+				std::cout << "Unexpected Chunk: " << child->GetType() << "\n";
+			}
+		}
+
+		return std::make_unique<CompositeDrawablePropList>(std::move(props));
+	}
+
+	std::unique_ptr<CompositeDrawableProp> CompositeDrawableProp::Load(const P3DChunk& chunk)
+	{
+		assert(chunk.IsType(ChunkType::CompositeDrawableProp));
+
+		MemoryStream stream(chunk.GetData());
+
+		auto name = stream.ReadLPString();
+		auto skeletonJoint = stream.Read<uint32_t>();
+
+		for (auto const& child : chunk.GetChildren())
+		{
+			switch (child->GetType())
+			{
+			case ChunkType::CompositeDrawableSortOrder:
+				break;
+			default:
+				std::cout << "Unexpected Chunk: " << child->GetType() << "\n";
+			}
+		}
+
+		return std::make_unique<CompositeDrawableProp>(name, skeletonJoint);
 	}
 } // namespace Donut::P3D
--- src/P3D/CompositeDrawable.h
+++ src/P3D/CompositeDrawable.h
@@ -4,11 +4,51 @@
 
 namespace Donut::P3D
 {
+	class CompositeDrawableProp
+	{
+	public:
+
+		CompositeDrawableProp(const std::string& name, uint32_t skeletonJoint) :
+			_name(name),
+			_skeletonJoint(skeletonJoint)
+		{
+		}
+
+		static std::unique_ptr<CompositeDrawableProp> Load(const P3DChunk&);
+
+	private:
+
+		std::string _name;
+		uint32_t _skeletonJoint;
+	};
+
+	class CompositeDrawablePropList
+	{
+	public:
+
+		CompositeDrawablePropList(std::vector<std::unique_ptr<CompositeDrawableProp>> props) :
+			_props(std::move(props))
+		{
+		}
+
+		static std::unique_ptr<CompositeDrawablePropList> Load(const P3DChunk&);
+
+	private:
+
+		std::vector<std::unique_ptr<CompositeDrawableProp>> _props;
+	};
+
 	class CompositeDrawable
 	{
 	public:
 
-		CompositeDrawable()
+		CompositeDrawable(
+			const std::string& name,
+			const std::string& skeletonName,
+			std::unique_ptr<CompositeDrawablePropList> propList) :
+			_name(name),
+			_skeletonName(skeletonName),
+			_propList(std::move(propList))
 		{
 		}
 
@@ -16,5 +56,8 @@ namespace Donut::P3D
 
 	private:
 
+		std::string _name;
+		std::string _skeletonName;
+		std::unique_ptr<CompositeDrawablePropList> _propList;
 	};
 } // namespace Donut::P3D
