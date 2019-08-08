93947d5cfbc5be5ab76858b8afd534986502ae0c
aylaylay <fortblox@gmail.com>
2019-08-08 03:09:01 +0100

    update codegen

---
+++ dev/codegen/run.bat
@@ -0,0 +1,3 @@
+echo Generating Code
+
+donut-codegen --p3din "p3d.json" --p3dout "../../src/P3D" --cmdin "cmd.json" --cmdout "../../src" --copyright "Copyright 2019 the donut authors. See AUTHORS.md"
--- dev/p3dcppgen/run.bat
+++
@@ -1,3 +0,0 @@
-echo Generating P3D Loaders
-
-p3dcppgen -i "../../src/P3D/p3d.json" -o "../../src/P3D"
