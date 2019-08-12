c6780c5ee6e197f5e9fa7d456a364ad801f987dc
aylaylay <fortblox@gmail.com>
2019-08-13 00:42:40 +0100

    fix cmd out path

--- dev/codegen/run.bat
+++ dev/codegen/run.bat
@@ -1,4 +1,4 @@
 echo Generating Code
 
 Pushd "%~dp0\.."
-"codegen/donut-codegen.exe" --p3din "codegen/p3d.json" --p3dout "../src/P3D" --cmdin "codegen/cmd.json" --cmdout "../src" --copyright "Copyright 2019 the donut authors. See AUTHORS.md"
+"codegen/donut-codegen.exe" --p3din "codegen/p3d.json" --p3dout "../src/P3D" --cmdin "codegen/cmd.json" --cmdout "../src/Scripting" --copyright "Copyright 2019 the donut authors. See AUTHORS.md"
