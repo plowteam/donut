b4dd1e5b675573127893c4c1221acc57a63a8353
Matt Stevens <matt@handsomematt.co.uk>
2020-01-06 22:22:13 +0000

    imgui: Add ImVec2 <-> Donut::Vector2 to IM_VEC2_CLASS_EXTRA

--- src/Render/imgui/imconfig.h
+++ src/Render/imgui/imconfig.h
@@ -55,11 +55,14 @@
 
 //---- Define constructor and implicit cast operators to convert back<>forth between your math types and ImVec2/ImVec4.
 // This will be inlined as part of ImVec2 and ImVec4 class declarations.
-/*
-#define IM_VEC2_CLASS_EXTRA                                                 \
-        ImVec2(const MyVec2& f) { x = f.x; y = f.y; }                       \
-        operator MyVec2() const { return MyVec2(x,y); }
 
+#include "Core/Math/Vector2.h"
+
+#define IM_VEC2_CLASS_EXTRA                                                 \
+        ImVec2(const Donut::Vector2& f) { x = f.X; y = f.Y; }                       \
+        operator Donut::Vector2() const { return Donut::Vector2(x, y); }
+
+/*
 #define IM_VEC4_CLASS_EXTRA                                                 \
         ImVec4(const MyVec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }     \
         operator MyVec4() const { return MyVec4(x,y,z,w); }
